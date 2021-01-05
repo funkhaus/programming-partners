e1344553db2e869557aff57e0b54739cdeda3ffd
dChiamp <derek.chiampas@gmail.com>
2021-01-04 16:26:04 -0800

    adds basic global-logo component

---
+++ components/global/Logo.vue
@@ -0,0 +1,38 @@
+<template lang="html">
+    <nuxt-link
+        to="/"
+        class="global-logo"
+    >
+        <svg-logo-funkhaus class="svg" />
+    </nuxt-link>
+</template>
+
+<script>
+import SvgLogoFunkhaus from "~/assets/svg/logo-funkhaus"
+
+export default {
+    components: {
+        SvgLogoFunkhaus,
+    },
+}
+</script>
+
+<style lang="scss" scoped>
+.global-logo {
+    .svg {
+        height: 20px;
+        width: auto;
+        transition: height 0.4s ease-in-out;
+    }
+    path {
+        fill: var(--color-yellow);
+    }
+
+    // Breakpoints
+    @media #{$lt-phone} {
+        .svg {
+            height: 15px;
+        }
+    }
+}
+</style>
