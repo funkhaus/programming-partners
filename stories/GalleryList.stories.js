af3d1eb19e849c27db2c215a48be7041fdd1b10c
Nick Berens <nickberens360@gmail.com>
2021-09-16 15:00:44 -0500

    add 404 error story

--- stories/GalleryList.stories.js
+++ stories/GalleryList.stories.js
@@ -1,5 +1,6 @@
 import GalleryList from "~/components/gallery/List"
 import WpImage from "~/components/WpImage"
+import { data as API_IMAGE_404 } from "~/stories/mock-api-error.json"
 import { data as API } from "~/stories/mock-api.json"
 
 export default {
@@ -10,6 +11,20 @@ export const Default = () => ({
     data() {
         return {
             items: API.directors.children.nodes,
+        }
+    },
+    components: {
+        GalleryList,
+        WpImage,
+    },
+    template: `
+        <gallery-list :items="items"/>`,
+})
+
+export const Image404 = () => ({
+    data() {
+        return {
+            items: API_IMAGE_404.directors.children.nodes,
         }
     },
     components: {
